f97b126a9d513fd9eef7769e98b3312c954f2e68
erigro11 <101679354+erigro11@users.noreply.github.com>
2022-03-28 10:02:10 +1300

    Add files via upload

---
+++ 03_getting_imput.py
@@ -0,0 +1,15 @@
+username = input("What is Your Name? ")
+
+fav_num = int(input("What is Your Favourite Number? "))
+
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print()
+print("hi {}, your favourite number is {}" .format(username, fav_num))
+print()
+print("double {} is {}" .format(fav_num, double))
+print("Half {} is {}" .format(fav_num, half))
+print("{} squared is {}" .format(fav_num, squared))
+print()
---
+++ 04_GK_try_except.py
@@ -0,0 +1,4 @@
+try:
+    response = int(input("Enter a number"))
+except ValueError:
+    print("you did not enter a number")
